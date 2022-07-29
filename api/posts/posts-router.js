b377c3b898fcc423830242136906548b9c66b764
Melissa <wolfaholic@users.noreply.github.com>
2022-07-29 00:07:25 -0700

    WIP - 15 passing

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -78,9 +78,42 @@ router.delete('/:id', async (req, res) => {
         })
     }
 })
-// router.put('/:id', (req, res) => {
-
-// })
+router.put('/:id', (req, res) => {
+    const { title, contents } = req.body
+    if (!title || !contents) {
+        res.status(400).json({
+            message: "Please provide title and contents for the post" 
+        })
+    } else {
+        Post.findById(req.params.id)
+            .then(updatePost => {
+                if(!updatePost) {
+                    res.status(404).json({
+                        message: "The post with the specified ID does not exist"
+                    })
+                } else {
+                    return Post.update(req.params.id, req.body)
+                }
+            })
+            .then(data => {
+                if(data){
+                    return Post.findById(req.params.id)
+                }
+            })
+            .then(post => {
+                if (post){
+                    res.json(post)
+                }
+            })
+            .catch(err => {
+                res.status(500).json({
+                    message: "The post information could not be modified",
+                    err: err.message,
+                    stack: err.stack,
+                })
+            })
+    }
+})
 // router.get('/:id/comments', (req, res) => {
 
 // })
