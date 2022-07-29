466a21d9f925c77bfd27eab2cbd44236bad15253
Melissa <wolfaholic@users.noreply.github.com>
2022-07-28 22:24:41 -0700

    WIP - 5 Tests Passing

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -18,9 +18,24 @@ router.get('/', (req, res) => {
             })
         })
 })
-// router.get('/:id', (req, res) => {
-
-// })
+router.get('/:id', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        if(!post) {
+            res.status(404).json({
+                message: "The post with the specified ID does not exist",
+            })
+        } else {
+            res.json(post)
+        }
+    } catch(err){
+        res.status(500).json({
+            message: "The post information could not be retrieved",
+            err: err.message,
+            stack: err.stack,
+        })
+    }
+})
 // router.post('/', (req, res) => {
 
 // })
